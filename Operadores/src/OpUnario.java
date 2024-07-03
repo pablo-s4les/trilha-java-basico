7141ed6c00312edbba01c822e8d3c609acee15bc
pablo sales <pablogsales87@outlook.com>
2024-07-03 18:32:18 -0300

    Operadores unários

---
+++ Operadores/src/OpUnario.java
@@ -0,0 +1,29 @@
+public class OpUnario {
+    public static void main(String[] args) {
+        boolean variavel = true;
+        variavel = !variavel;
+            System.out.println(variavel);
+
+        int numero = 5;
+
+        numero ++;
+            System.out.println(numero);
+
+        numero = numero +1;
+            System.out.println(numero);
+
+            System.out.println(numero --);
+
+            System.out.println(numero);
+
+        numero = - numero;
+            System.out.println(numero);
+
+        numero = numero * -1;
+            System.out.println(numero);
+
+          
+
+    }
+    
+}
