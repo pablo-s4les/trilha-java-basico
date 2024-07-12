a5d3c0613d6cd7d01c4c06b073bee6fb4f1a6e7a
pablo-s4les <pablogsales87@outlook.com>
2024-07-12 08:46:26 -0300

    Aula condicional ternário exemplo um

---
+++ controle-fluxo/src/EscolarTernarioUm.java
@@ -0,0 +1,8 @@
+public class EscolarTernarioUm {
+    public static void main(String[] args) {
+        int nota = 5;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+            System.out.println(resultado);
+
+    }
+}
