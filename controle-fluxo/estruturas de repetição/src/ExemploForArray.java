d50f4ed93801a38b4e850e043c0a33288246fdd1
pablo sales <pablogsales87@outlook.com>
2024-07-13 10:16:13 -0300

    Aula de estrutura de repetição com for array

---
+++ controle-fluxo/estruturas de repetição/src/ExemploForArray.java
@@ -0,0 +1,14 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        //Em Arrays o indice inicia em ZERO/0
+        String alunos [] = {"PABLO", "AURISTELA", "PAULO", "PATROCK"};
+
+        for (int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+        
+
+    }
+
+
+}
