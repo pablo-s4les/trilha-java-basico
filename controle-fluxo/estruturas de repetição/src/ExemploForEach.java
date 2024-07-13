31ca18654a4af22223e6a0ad8d58f62a1b7789b1
pablo sales <pablogsales87@outlook.com>
2024-07-13 10:16:44 -0300

    Aula com For Each

---
+++ controle-fluxo/estruturas de repetição/src/ExemploForEach.java
@@ -0,0 +1,10 @@
+public class ExemploForEach {
+    public static void main(String[] args) {
+        String alunos [] = {"PABLO", "AURISTELA", "PAULO", "PATROCK"};
+
+         //Forma abreviada
+        for(String aluno : alunos) {
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
