217a3e4372525d8779f40f3e86794187f46410c7
pablo-s4les <pablogsales87@outlook.com>
2024-07-26 11:28:04 -0300

    Código do desafio de verificação de cheque especial

---
+++ Desafios/VerificacaoChequeEspecial.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class VerificacaoChequeEspecial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+        double saque = scanner.nextDouble();
+
+        // Define um limite para o cheque especial
+        double limiteChequeEspecial = 500;
+
+        // Verifica se o saque ultrapassa o saldo disponível
+        if (saque <= saldo) {
+            // TODO: Imprimir "Transação realizada com sucesso."
+            System.out.println("Transacao realizada com sucesso.");
+        }else if(Math.abs(saldo - saque) <= limiteChequeEspecial){
+            // TODO: Verificar se o saque ultrapassa o limite do cheque especial
+            System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+
+        }else{
+            System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
+        }
+        scanner.close();
+        }
+
+       
+    }
